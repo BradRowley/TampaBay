da397bbd0add2c761a3fb774a6316edd69b22a86
Brad Rowley <Bradleyrowley@yahoo.com>
2020-10-19 14:15:48 -0400

    database/seeds

---
+++ Models/seeds.sql
@@ -0,0 +1,5 @@
+TRUNCATE TABLE "Events";
+
+INSERT INTO "Events" ("Name", "Description", "Requirements","Cost", "Category","Address") VALUES ('Bobs Burgers', 'Great place to get a quick snack!','N/A', 15, 'Food', '9100 Tampa Rd, Tampa, FL 33634');
+INSERT INTO "Events" ("Name", "Description", "Requirements","Cost", "Category","Address") VALUES ('Rowdys', 'Great for dancing!','21 and up',10, 'Night life', '1000 Roosevelt Blvd, Clearwater, FL 33760');
+INSERT INTO "Events" ("Name", "Description", "Requirements","Cost", "Category","Address") VALUES ('Shady Swings', 'Fun place to ride some awesome rides!','Height requirments for specific rides',50,'Outdoors' , '1002 Jensen Rd, Clearwater, FL 33760');
